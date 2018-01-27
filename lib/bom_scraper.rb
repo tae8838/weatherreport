cceb22652c0f7b6efdd3390fac7fae4dabc22e4c
Noppakun Wongsrinoppakun <tae8838@gmail.com>
2018-01-27 18:47:07 +0700

    updated bom_scraper to just parse data

--- lib/bom_scraper.rb
+++ lib/bom_scraper.rb
@@ -6,28 +6,26 @@ URL = 'http://www.bom.gov.au/vic/observations/melbourne.shtml'
 module BomScraper
   def self.generate_readings
     doc = Nokogiri::HTML(open(URL))
+    output_hash = {}
 
     doc.css('tbody tr').each do |location|
       name = location.children[1].text
-      datetime = location.children[3].text
+      time = location.children[3].text.match(/[0-9][0-9]\:[0-9][0-9][a-z][a-z]/).to_s
       temperature = location.children[5].text
       dew_point = location.children[9].text
       wind_speed = location.children[17].text
       wind_direction = location.children[15].text
       rainfall_amount = location.children[27].text
 
-      weather_station = WeatherStation.find_or_create_by(name: name)
+      output_hash[name] = {time: time,
+                           temperature: temperature,
+                           dew_point: dew_point,
+                           wind_speed: wind_speed,
+                           wind_direction: wind_direction,
+                           rainfall_amount: rainfall_amount}
 
-      reading = weather_station.readings.new
-
-      reading.update_attributes(day: datetime,
-                                time: datetime,
-                                temperature: temperature,
-                                dew_point: dew_point,
-                                wind_speed: wind_speed,
-                                wind_direction: wind_direction,
-                                rainfall_amount: rainfall_amount)
-      reading.save!
     end
+
+    output_hash
   end
 end
