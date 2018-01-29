04d5363770c60ee64a79aa57ec32273f07b6c824
Noppakun Wongsrinoppakun <tae8838@gmail.com>
2018-01-29 15:15:25 +0700

    explicit require

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,6 @@ module MelbourneWeather
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.autoload_paths += %W(#{config.root}/lib)
+    require Rails.root.join('lib/bom_scraper')
   end
 end
